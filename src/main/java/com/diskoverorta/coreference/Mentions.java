84b8f8aa6ee025a6ae6a6b36dcdde710510c38ae
naren <makernaren@gmail.com>
2015-01-22 15:40:43 +0530

    Add person,org entity filter to keys in mentions map

--- src/main/java/com/diskoverorta/coreference/Mentions.java
+++ src/main/java/com/diskoverorta/coreference/Mentions.java
@@ -21,7 +21,7 @@ public class Mentions {
 
     static StanfordNLP nlpStanford = new StanfordNLP();
 
-    public static void getMentions(List<String> entities ) {
+    public static void getMentions(Set<String> entities ) {
         // read some text in the text variable
         String text = "Teresa h meng founded Atheros communications. Meng served on the board of Atheros. Charles barratt was the CEO of Atheros inc., Barratt and Meng were directors of Atheros.";
 
@@ -36,6 +36,7 @@ public class Mentions {
         //List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
 
         Map<Integer, CorefChain> graph = document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
+        Map<String, Set<String>> mp = new HashMap<>();
         for (Integer temp : graph.keySet()) {
 
             CorefChain c = graph.get(temp);
@@ -47,36 +48,37 @@ public class Mentions {
 
             // remove "in Sentences"
             for (CorefChain.CorefMention each : cms) {
-//
-//                String s = each.toString().split("\"")[0];
-//                cms_parsed.add(s);
 
-//                System.out.println(each.toString());
                 Pattern p = Pattern.compile("\"([^\"]*)\"");
                 Matcher m = p.matcher(each.toString());
                 while (m.find()) {
-
 //                    System.out.println(m.group(1));
                     cms_parsed.add(m.group(1));
                 }
             }
-            
 
-            Map<String, Set<String>> mp = new HashMap<>();
-            Set tmp = new HashSet();
-            for (String element : cms_parsed.subList(1, cms.size())) {
-                tmp.add(element);
+            //put it into a map container.
+
+            if(entities.contains(cms_parsed.get(0).trim().toLowerCase())) {
+                Set<String> tmp = new HashSet<>();
+                for (String element : cms_parsed.subList(1, cms.size())) {
+                    tmp.add(element);
+                }
+                mp.put(cms_parsed.get(0), tmp);
+
             }
-            mp.put(cms_parsed.get(0), tmp);
 
-            System.out.println("Mentions:  ");
+
 //            System.out.println(cms);
-            System.out.println(mp);
-            System.out.println(" ");
+//            System.out.println("Mentions:  ");
+//            System.out.println(mp);
+
         }
+        System.out.println("Mentions:  ");
+        System.out.println(mp);
     }
 
-    public static List<String> getEntity() {
+    public static Set<String> getEntity() {
 
         EntityManager entity = new EntityManager();
         Map<String,String> entityConfig = new HashMap<>();
@@ -84,10 +86,12 @@ public class Mentions {
         entityConfig.put("Organization","TRUE");
 
         EntityObject en = entity.getSelectedEntitiesForSentence("Teresa H Meng founded Atheros communications. Meng served on the board of Atheros. Charles barratt was the CEO of Atheros inc., Barratt and Meng were directors of Atheros.", entityConfig);
-        System.out.println(en.person);
-        System.out.println(en.organization);
-        List<String> entities = en.person;
-        entities.addAll(en.organization);
+//        System.out.println(en.person);
+//        System.out.println(en.organization);
+        Set<String> entities = new HashSet<>();
+        List<String> entities_raw = en.person;
+        entities_raw.addAll(en.organization);
+
         // process the organisation entity string
 //        for(String element : en.organization)
 //        {
@@ -95,14 +99,21 @@ public class Mentions {
 //            entities.add(s);
 //        }
 
+        //trim and lower case
+        for(String element : entities_raw)
+        {
+            entities.add(element.trim().toLowerCase());
+
+        }
         System.out.println("Total entities" + entities);
-        return entities;
+       return entities;
 
     }
 
     public static void main(String args[])
     {
         getMentions(getEntity());
+//        getEntity();
 
     }
 
