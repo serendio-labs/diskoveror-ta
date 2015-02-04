9b8ddb6ec6429653a0b73ba59adb91d19267c22c
naren <makernaren@gmail.com>
2015-02-04 16:21:53 +0530

    Call method to close database connection

--- src/main/java/com/diskoverorta/ontology/OntologyLookup.java
+++ src/main/java/com/diskoverorta/ontology/OntologyLookup.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 
 public class OntologyLookup
 {
-    Connection m_con = null;
+    static Connection m_con = null;
     static Statement m_stmt = null;
 
     OntologyLookup()
@@ -27,33 +27,12 @@ public class OntologyLookup
         String password = null;
         try
         {
-//            String sdoc = Files.toString(new File("dbconnection.prop"), Charset.defaultCharset());
-//            String docs[] = sdoc.split("\n");
-//            for(int i = 0; i < docs.length; i++)
-//            {
-//                String options[] = docs[i].split(" : ");
-//                if(options[0].equals("Database Address") == true)
-//                    ipAddress = options[1];
-//                if(options[0].equals("Database Name") == true)
-//                    dbName = options[1];
-//                if(options[0].equals("Username") == true)
-//                    username = options[1];
-//                if(options[0].equals("Password") == true)
-//                    password = options[1];
-//            }
-//            if((ipAddress == null) || (dbName == null) || (username == null) || (password == null))
-//            {
-//                System.out.println("Database details not present.");
-//                return;
-//            }
+
             Class.forName("org.postgresql.Driver");
             m_con = DriverManager.getConnection("jdbc:postgresql://" + "178.63.22.132" + "/" + "vipin_test","amc_engineer","serendio123");
             m_stmt = m_con.createStatement();
         }
-//        catch (IOException ex)
-//        {
-//            ex.printStackTrace();
-//        }
+
         catch (ClassNotFoundException ex)
         {
             ex.printStackTrace();
@@ -64,70 +43,7 @@ public class OntologyLookup
         }
     }
 
-    void insertFuzzySets(String sent1No,String sent2No,ArrayList<String> verb1,ArrayList<String> verb2,String simScore)
-    {
 
-        String verb1_list = "";
-        String verb2_list = "";
-        String valid = "false";
-        for(int i = 0;(verb1!=null)&&(i < verb1.size());i++)
-        {
-            if(i == (verb1.size()-1))
-                verb1_list = verb1_list + "\""+verb1.get(i)+"\"";
-            else
-                verb1_list = verb1_list + "\""+verb1.get(i)+"\""+",";
-        }
-        for(int i = 0;(verb2!=null)&&(i < verb2.size());i++)
-        {
-            if(i == (verb2.size()-1))
-                verb2_list = verb2_list + "\""+verb2.get(i)+"\"";
-            else
-                verb2_list = verb2_list + "\""+verb2.get(i)+"\""+",";
-        }
-
-        if(simScore.length() > 4)
-            simScore = simScore.substring(0,3);
-
-        if(Double.parseDouble(simScore) >= 0.5)
-            valid = "true";
-
-        verb1_list = verb1_list.replace("'"," ");
-        verb2_list = verb2_list.replace("'"," ");
-
-
-
-        String query = "insert into fuzzy_group (sentences,sentence1_verbs,sentence2_verbs,similarity_score,ismatch) values('{"+sent1No+","+sent2No+"}','{"+verb1_list+"}','{"+verb2_list+"}',"+simScore+","+valid+")";
-        try
-        {
-            m_stmt.executeUpdate(query);
-        }
-        catch(SQLException ex)
-        {
-            ex.printStackTrace();
-        }
-    }
-    void insertFuzzySets1(String sent1No,String sent2No,String simScore)
-    {
-
-        String valid = "false";
-
-        if(simScore.length() > 4)
-            simScore = simScore.substring(0,3);
-
-        if(Double.parseDouble(simScore) >= 0.5)
-            valid = "true";
-
-
-        String query = "insert into fuzzy_group (sentences,similarity_score) values('{"+sent1No+","+sent2No+"}',"+simScore+")";
-        try
-        {
-            m_stmt.executeUpdate(query);
-        }
-        catch(SQLException ex)
-        {
-            ex.printStackTrace();
-        }
-    }
     static Map<String, Set<String>> getTermsFromTable(String kind)
     {
         Map<String, Set<String>> doc_Sentences = new HashMap<>();
@@ -154,7 +70,7 @@ public class OntologyLookup
         }
         return doc_Sentences;
     }
-    void closeConnection()
+    static void closeConnection()
     {
         try
         {
@@ -186,6 +102,7 @@ public class OntologyLookup
             }
             result_map.put(key, result);
         }
+        closeConnection();
         return result_map;
 
     }
