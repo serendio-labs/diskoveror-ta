473f5fab210da032b2ab5e0786f7cd06dc934e29
naren <makernaren@gmail.com>
2015-02-04 14:06:24 +0530

    Add OntologyManager to call OntologyLookup

--- src/main/java/com/diskoverorta/ontology/OntologyManager.java
+++ src/main/java/com/diskoverorta/ontology/OntologyManager.java
@@ -1,11 +1,23 @@
 package com.diskoverorta.ontology;
 
+import com.diskoverorta.vo.TAConfig;
+
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by naren on 4/2/15.
  */
 public class OntologyManager {
 
+    OntologyLookup terms = new OntologyLookup();
+    public static Map<String, Set<String>> getOntologyForSelectedEntites(String content, Map<String,String> ontologyConfig)
+    {
+        TAConfig config = new TAConfig();
+        String data = "Interrogatories interrograted the victim which was Petty offense as the victim had already went" +
+                "through a Mental health treatment suggested by Senior Judge ";
 
+    }
 
 
 }
