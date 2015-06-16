9befef91c3bb37a0ff5213ef31f87f467c36d875
praveen <pjesudhas@yahoo.com>
2015-06-16 16:50:14 +0530

    Text and sentiment final integration

---
+++ src/main/java/com/diskoverorta/vo/TextInformation.java
@@ -0,0 +1,17 @@
+package com.diskoverorta.vo;
+
+import java.util.Set;
+
+/**
+ * Created by praveen on 16/6/15.
+ */
+public class TextInformation
+{
+    public Set<String> topics = null;
+    public Set<String> sentiment = null;
+
+    public TextInformation()
+    {
+
+    }
+}
