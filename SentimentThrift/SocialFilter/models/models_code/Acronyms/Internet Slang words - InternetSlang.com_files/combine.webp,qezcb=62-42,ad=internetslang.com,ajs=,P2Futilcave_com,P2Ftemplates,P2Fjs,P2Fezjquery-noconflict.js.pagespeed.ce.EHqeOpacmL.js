bf92a0191e3f26f100b9ea7d9769fe98b9a56cbc
praveen <pjesudhas@yahoo.com>
2015-07-01 15:38:35 +0530

    Removed unneccesary files from repository

--- SentimentThrift/SocialFilter/models/models_code/Acronyms/Internet Slang words - InternetSlang.com_files/combine.webp,qezcb=62-42,ad=internetslang.com,ajs=,P2Futilcave_com,P2Ftemplates,P2Fjs,P2Fezjquery-noconflict.js.pagespeed.ce.EHqeOpacmL.js
+++
@@ -1,2 +0,0 @@
-if(typeof $ezJQuery=='undefined')
-{var $ezJQuery=jQuery.noConflict(true);}
--- TopicThrift/categorizer.thrift
+++
@@ -1,7 +0,0 @@
-namespace py categorizer
-
-service Categorizer {
-  void ping(),
-  string getTopic(1:string msg)
-}
-
